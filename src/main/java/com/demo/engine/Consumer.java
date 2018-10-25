54344ad3d55b2d7b19f8a85ebc635cbbbeec0b4f
ihor.kosandyak@orilinc.com <ihor.kosandyak@orilinc.com>
2018-10-25 20:49:45 +0300

    Removed unused ObjectMapper in Consumer

--- src/main/java/com/demo/engine/Consumer.java
+++ src/main/java/com/demo/engine/Consumer.java
@@ -1,9 +1,7 @@
 package com.demo.engine;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
@@ -13,8 +11,6 @@ import java.io.IOException;
 public class Consumer {
 
     private final Logger logger = LoggerFactory.getLogger(Producer.class);
-    @Autowired
-    private final ObjectMapper mapper = new ObjectMapper();
 
     @KafkaListener(topics = "users", groupId = "group_id")
     public void consume(String message) throws IOException {
